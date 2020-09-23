892774c2b646a175a5d537b64c66515e97cbd7d2
LsHallo <xlshallox@gmail.com>
2020-09-23 14:28:10 +0200

    initial commit; it shows a window

---
+++ src/anno.rs
@@ -0,0 +1,71 @@
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::{timing::Time, transform::Transform},
+    ecs::{Component, DenseVecStorage, Entity},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    ui::{Anchor, LineMode, TtfFormat, UiText, UiTransform},
+};
+
+#[derive(Default)]
+pub struct Anno {
+    //ball_spawn_timer: Option<f32>,
+    sprite_sheet_handle: Option<Handle<SpriteSheet>>,
+}
+
+impl SimpleState for Anno {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        // Wait one second before spawning the ball.
+        //self.ball_spawn_timer.replace(1.0);
+
+        // Load the spritesheet necessary to render the graphics.
+        // `spritesheet` is the layout of the sprites on the image;
+        // `texture` is the pixel data.
+        //self.sprite_sheet_handle.replace(load_sprite_sheet(world));
+
+        initialise_camera(world);
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        Trans::None
+    }
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron", // Here we load the associated ron file
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
+}
+
+fn initialise_camera(world: &mut World) {
+    // Setup camera in a way that our screen covers whole arena and (0, 0) is in the bottom left.
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(50., 50., 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(100., 100.))
+        .with(transform)
+        .build();
+}
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use amethyst::{
+    audio::AudioBundle,
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+
+mod anno;
+use crate::anno::Anno;
+
+fn main() -> amethyst::Result<()> {
+    //Setup logger
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.005, 0.0, 0.007, 1.0]), //background color rgba
+                )
+                // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderUi::default()),
+        )?
+        // Add the transform bundle which handles tracking entity positions
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(AudioBundle::default())?;
+    //.with(systems::PaddleSystem, "paddle_system", &["input_system"])
+    //.with(systems::MoveBallsSystem, "ball_system", &[])
+    //.with(
+    //    systems::BounceSystem,
+    //    "collision_system",
+    //    &["paddle_system", "ball_system"],
+    //)
+    //.with(systems::WinnerSystem, "winner_system", &["ball_system"]);
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Anno::default(), game_data)?;
+    game.run();
+    Ok(())
+}
